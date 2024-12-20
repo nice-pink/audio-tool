91f9402e440f3a6a9e8a9ea14740b24be9991691
nice-pink <r@nice.pink>
2024-12-20 15:24:04 +0100

    feat: Audio file contains size.

--- pkg/audio/audio.go
+++ pkg/audio/audio.go
@@ -9,12 +9,20 @@ import (
 
 type AudioFile struct {
 	Filepath string
+	Size     int64
 	Data     []byte
 	Infos    *encodings.AudioInfos
 }
 
 func NewAudioFile(filepath string, loadData, parse bool) *AudioFile {
 	audio := &AudioFile{Filepath: filepath}
+
+	fs, err := os.Stat(filepath)
+	if err != nil {
+		log.Err(err, "file stats", filepath)
+		return nil
+	}
+	audio.Size = fs.Size()
 
 	if loadData || parse {
 		data, err := os.ReadFile(filepath)
