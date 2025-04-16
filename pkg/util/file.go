1c14b9c0930799c06e586fcb523bb3eb1c1230e9
nice-pink <r@nice.pink>
2025-04-16 13:44:40 +0200

    feat: file download and clean up.

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -1,8 +1,14 @@
 package util
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"strconv"
 	"time"
+
+	"github.com/nice-pink/goutil/pkg/filesystem"
 )
 
 func GetFilePath(baseFilePath string) string {
@@ -11,4 +17,50 @@ func GetFilePath(baseFilePath string) string {
 	}
 	now := time.Now()
 	return baseFilePath + "_" + strconv.FormatInt(now.Unix(), 10)
+}
+
+func CleanUp(folder string, sec int64, ignoreHiddenFiles bool, delete bool) {
+	// get files
+	files := filesystem.ListFiles(folder, sec, ignoreHiddenFiles)
+
+	// logs
+	ago := -time.Duration(sec) * time.Second
+	dateThreshold := time.Now().Add(ago)
+	fmt.Println("Files older than:", dateThreshold)
+	for _, file := range files {
+		fmt.Println(file)
+	}
+	fmt.Println(strconv.Itoa(len(files)), "files")
+	fmt.Println()
+
+	// delete
+	if delete {
+		fmt.Println("Delete files...")
+		filesystem.DeleteFiles(files)
+		fmt.Println("Done!")
+	}
+}
+
+func DownloadFile(url string, filepath string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
