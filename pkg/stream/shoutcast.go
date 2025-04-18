9a3ee082ebb3aafd80a1bac33934f234722d3ca8
nice-pink <r@nice.pink>
2025-04-18 14:09:34 +0200

    fix: header.

--- pkg/stream/shoutcast.go
+++ pkg/stream/shoutcast.go
@@ -2,7 +2,7 @@ package stream
 
 func GetShoutcastSourceHeader(connTarget ConnTarget, httpVersion string, print bool) ([]byte, error) {
 	header := "SOURCE " + connTarget.MountPoint + " HTTP/" + httpVersion + "\r\n"
-	header += "Host: " + connTarget.Domain + ":" + connTarget.Port + "\r\n"
+	header += "Host: " + connTarget.Domain + "\r\n"
 	header += "User-Agent: " + connTarget.UserAgent + "\r\n"
 	if connTarget.BasicAuth != "" {
 		header += "Authorization: Basic " + connTarget.BasicAuth + "\r\n"
